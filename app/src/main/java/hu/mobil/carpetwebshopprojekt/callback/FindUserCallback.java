2d1ff40516f777abd1330cd7d3b1e9b7d5b58c09
Joe <66317857+RigoLa133@users.noreply.github.com>
2024-04-13 15:53:57 +0200

    Delete app directory

--- app/src/main/java/hu/mobil/carpetwebshopprojekt/callback/FindUserCallback.java
+++
@@ -1,7 +0,0 @@
-package hu.mobil.carpetwebshopprojekt.callback;
-
-import com.google.firebase.firestore.DocumentReference;
-
-public interface FindUserCallback {
-    void onUserFound(DocumentReference userRef);
-}
